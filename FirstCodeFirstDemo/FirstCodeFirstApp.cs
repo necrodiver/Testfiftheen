3bdacecb87655aebff8a50ebfd80e36f3b8ae8a4
necrodiver <neclodiver@live.com>
2017-06-28 11:43:37 +0800

    创建EF CodeFirst的Demo

---
+++ FirstCodeFirstDemo/FirstCodeFirstApp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace FirstCodeFirstDemo
+{
+    public class FirstCodeFirstApp
+    {
+        public class Context : DbContext
+        {
+            public Context() : base("name=FirstCodeFirstApp")
+            {
+
+            }
+            public DbSet<Donator> Donators { get; set; }
+        }
+    }
+    public class Donator
+    {
+        [Key]
+        public int DonatorId { get; set; }
+        [StringLength(200, MinimumLength = 2)]
+        public string Name { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime DonateDate { get; set; }
+    }
+
+}
--- FirstCodeFirstDemo/Program.cs
+++ FirstCodeFirstDemo/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static FirstCodeFirstDemo.FirstCodeFirstApp;
 
 namespace FirstCodeFirstDemo
 {
@@ -10,6 +11,35 @@ namespace FirstCodeFirstDemo
     {
         static void Main(string[] args)
         {
+            using (var context = new Context())
+            {
+                context.Database.CreateIfNotExists();//如果数据库不存在时则创建
+                var donators = new List<Donator>
+                {
+                    new Donator
+                    {
+                        Name="张三",
+                        Amount=50,
+                        DonateDate=new DateTime(2017,6,25)
+                    },
+                    new Donator
+                    {
+                        Name="李四",
+                        Amount=5,
+                        DonateDate=new DateTime(2017,6,26)
+                    },
+                    new Donator
+                    {
+                        Name="王五",
+                        Amount=18.8m,
+                        DonateDate=new DateTime(2017,6,28)
+                    }
+                };
+                context.Donators.AddRange(donators);
+                context.SaveChanges();
+            }
+            Console.WriteLine("DB has Created");//提示DB创建成功
+            Console.ReadKey();
         }
     }
 }
